954196ee040849ac103648273c6d4b07b1914bdf
@suraj-619 <suraj.nimsatkar4@gmail.com>
2026-01-18 21:53:06 +0530

    Initial Product microservice with Clean Architecture, EF Core, MediatR, filtering and pagination

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MiniMakers.Products.Application.Products.Queries;
+
+
+namespace MiniMakers.Products.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductsController(IMediator mediator)
+        {
+           _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] GetProductsQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MiniMakers.Products.Application.Products.Interfaces;
+using MiniMakers.Products.Infrastructure.Persistence;
+using MiniMakers.Products.Infrastructure.Repositories;
+using MiniMakers.Products.Application.Products.Queries;
+using FluentValidation;
+using MiniMakers.Products.Application.Products.Behaviors;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblyContaining<GetProductsQuery>();
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssemblyContaining<GetProductsQueryValidator>();
+
+builder.Services.AddDbContext<ProductsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Products API V1");
+    options.RoutePrefix = "swagger";
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
